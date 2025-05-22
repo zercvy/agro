95fb6914b48669d8e0b3bd4b2515ad2eef2dfac7
zercvy <isip_a.p.nesterov@mpt.ru>
2025-05-22 11:43:06 +0300

    о

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import { setAuthToken } from './api/axios'
+
 
 
 // 👉 восстановление токена при загрузке
 const token = localStorage.getItem('token')
-if (token) setAuthToken(token)
+
 const rootElement = document.getElementById('root')
 
 if (rootElement) {

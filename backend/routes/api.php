0a3db10da8b6863df24184358755d8f38710795c
zercvy <isip_a.p.nesterov@mpt.ru>
2025-05-22 13:45:31 +0300

    а

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,11 +2,11 @@
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisteredUserController;
 // Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
+Route::middleware('guest')->post('/register', [RegisteredUserController::class, 'store']);
 // Подтверждение email
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
